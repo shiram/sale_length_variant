0caff334c957f9b03f2707b87c77132a6b5169d5
Shirambere Edward <edward.shirambere@gmail.com>
2021-05-10 20:26:32 +0300

    remove price calculation from invoice

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from odoo.tools.misc import formatLang, format_date, get_lang
 
 
 class SaleOrderLine(models.Model):
@@ -62,114 +63,6 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     length = fields.Float(string='Length', digits=(3, 2))
-
-
-    def _get_price_total_and_subtotal(self, price_unit=None, quantity=None, discount=None, currency=None, product=None, partner=None, taxes=None, move_type=None):
-        self.ensure_one()
-        return self._get_price_total_and_subtotal_model(
-            price_unit=price_unit or self.price_unit,
-            quantity=((quantity if quantity else 0) * (self.length if (self.length and self.length > 0) else 1)) or (self.quantity * (self.length if (self.length and self.length > 0) else 1)),
-            discount=discount or self.discount,
-            currency=currency or self.currency_id,
-            product=product or self.product_id,
-            partner=partner or self.partner_id,
-            taxes=taxes or self.tax_ids,
-            move_type=move_type or self.move_id.move_type,
-        )
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        # OVERRIDE
-        ACCOUNTING_FIELDS = ('debit', 'credit', 'amount_currency')
-        BUSINESS_FIELDS = ('price_unit', 'quantity', 'discount', 'tax_ids')
-
-        for vals in vals_list:
-            move = self.env['account.move'].browse(vals['move_id'])
-            vals.setdefault('company_currency_id',
-                            move.company_id.currency_id.id)  # important to bypass the ORM limitation where monetary fields are not rounded; more info in the commit message
-
-            # Ensure balance == amount_currency in case of missing currency or same currency as the one from the
-            # company.
-            currency_id = vals.get('currency_id') or move.company_id.currency_id.id
-            if currency_id == move.company_id.currency_id.id:
-                balance = vals.get('debit', 0.0) - vals.get('credit', 0.0)
-                vals.update({
-                    'currency_id': currency_id,
-                    'amount_currency': balance,
-                })
-            else:
-                vals['amount_currency'] = vals.get('amount_currency', 0.0)
-
-            if move.is_invoice(include_receipts=True):
-                currency = move.currency_id
-                partner = self.env['res.partner'].browse(vals.get('partner_id'))
-                taxes = self.new({'tax_ids': vals.get('tax_ids', [])}).tax_ids
-                tax_ids = set(taxes.ids)
-                taxes = self.env['account.tax'].browse(tax_ids)
-
-                # Ensure consistency between accounting & business fields.
-                # As we can't express such synchronization as computed fields without cycling, we need to do it both
-                # in onchange and in create/write. So, if something changed in accounting [resp. business] fields,
-                # business [resp. accounting] fields are recomputed.
-                if any(vals.get(field) for field in ACCOUNTING_FIELDS):
-                    price_subtotal = self._get_price_total_and_subtotal_model(
-                        vals.get('price_unit', 0.0),
-                        (vals.get('quantity', 0.0) * (vals.get('length', 0.0) if vals.get('length', 0.0) > 0 else 1)),
-                        vals.get('discount', 0.0),
-                        currency,
-                        self.env['product.product'].browse(vals.get('product_id')),
-                        partner,
-                        taxes,
-                        move.move_type,
-                    ).get('price_subtotal', 0.0)
-                    vals.update(self._get_fields_onchange_balance_model(
-                        vals.get('quantity', 0.0),
-                        vals.get('discount', 0.0),
-                        vals['amount_currency'],
-                        move.move_type,
-                        currency,
-                        taxes,
-                        price_subtotal
-                    ))
-                    vals.update(self._get_price_total_and_subtotal_model(
-                        vals.get('price_unit', 0.0),
-                        (vals.get('quantity', 0.0) * (vals.get('length', 0.0) if vals.get('length', 0.0) > 0 else 1)),
-                        vals.get('discount', 0.0),
-                        currency,
-                        self.env['product.product'].browse(vals.get('product_id')),
-                        partner,
-                        taxes,
-                        move.move_type,
-                    ))
-                elif any(vals.get(field) for field in BUSINESS_FIELDS):
-                    vals.update(self._get_price_total_and_subtotal_model(
-                        vals.get('price_unit', 0.0),
-                        (vals.get('quantity', 0.0) * (vals.get('length', 0.0) if vals.get('length', 0.0) > 0 else 1)),
-                        vals.get('discount', 0.0),
-                        currency,
-                        self.env['product.product'].browse(vals.get('product_id')),
-                        partner,
-                        taxes,
-                        move.move_type,
-                    ))
-                    vals.update(self._get_fields_onchange_subtotal_model(
-                        vals['price_subtotal'],
-                        move.move_type,
-                        currency,
-                        move.company_id,
-                        move.date,
-                    ))
-
-        lines = super(AccountMoveLine, self).create(vals_list)
-
-        moves = lines.mapped('move_id')
-        if self._context.get('check_move_validity', True):
-            moves._check_balanced()
-        moves._check_fiscalyear_lock_date()
-        lines._check_tax_lock_date()
-        moves._synchronize_business_models({'line_ids'})
-
-        return lines
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
@@ -276,6 +169,5 @@ class SaleAdvancePaymentInv(models.TransientModel):
         if self._context.get('open_invoices', False):
             return sale_orders.action_view_invoice()
         return {'type': 'ir.actions.act_window_close'}
-
 
 
